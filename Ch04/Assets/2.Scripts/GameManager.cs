c166fde77eb35e20ba82e0cc783ac54e265c2f01
kmh <>
2026-04-01 14:58:48 +0900

    Ch04
    ch04

---
+++ Ch04/Assets/2.Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UIElements;
+
+public class GameManager : MonoBehaviour
+{
+    //public GameObject car;
+    //public GameObject flag;
+    //public GameObject distance;
+    public Transform car;
+    public Transform flag;
+    public TextMeshProUGUI distance;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //car = GameObject.Find("car"); ;
+
+        // Update is called once per frame
+    }
+    void Update()
+    {
+        //float length = flag.transform.position.x
+        //    - distance.transform.position.x;
+        float length = flag.position.x - car.position.x;
+        //distance.GetComponent<TextMeshProUGUI>().text =
+        //    "Distance : " + length.ToString("F2") + "m";
+        distance.text = "Distance : " + length.ToString("F2") + "m";
+    }
+}
