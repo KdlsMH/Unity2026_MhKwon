53fcd888de357470b79f87365f8e0104c3f5319e
kmh <>
2026-05-13 15:42:53 +0900

    0513-2
    0513-2

---
+++ CH08/Assets/20.Script/ItemController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    public float dropSpeed = -1f;
+
+    
+    void Update()
+    {
+        transform.Translate(0, dropSpeed * Time.deltaTime, 0);
+        if(transform.position.y < -5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
